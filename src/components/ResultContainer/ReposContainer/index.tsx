64a03996024f6d0ff6323e053cf9a8c99782126e
Jeffrey <matheusunitt@gmail.com>
2022-08-02 17:39:21 -0300

    add part of the required specific logic to the viewer profile page

--- src/components/ResultContainer/ReposContainer/index.tsx
+++ src/components/ResultContainer/ReposContainer/index.tsx
@@ -1,3 +1,4 @@
+import axios from "axios";
 import React, { useState, useEffect } from "react";
 
 import arrow_icon from "../../../assets/images/icons/arrow.png";
@@ -5,13 +6,54 @@ import arrow_icon from "../../../assets/images/icons/arrow.png";
 import "../styles.css";
 
 const ReposContainer = () => {
-  const [repositories, setRepositories] = useState([
-    "punkapi-api",
-    "poscard-8",
-    "python-mercadinho",
-  ]);
+  const [repositories, setRepositories] = useState([]);
+  const [username, setUsername] = useState("matheusunitt");
 
-  // useEffect(() => {}, []);
+  useEffect(() => {
+    const loadRepos = async () => {
+      await axios
+        .get(`https://api.github.com/users/${username}/repos`)
+        .then((response) => {
+          const data = response.data.map(
+            ({
+              id,
+              name,
+              language,
+              updated_at,
+              html_url,
+              created_at,
+              description,
+              forks_count,
+            }: {
+              id: number;
+              name: string;
+              language: string;
+              updated_at: string[];
+              html_url: string;
+              created_at: string;
+              description: string;
+              forks_count: number;
+            }) => {
+              return {
+                id,
+                name,
+                language,
+                updated_at,
+                html_url,
+                created_at,
+                description,
+                forks_count,
+              };
+            }
+          );
+
+          setRepositories(data);
+          console.log(data);
+        });
+    };
+
+    loadRepos();
+  }, []);
 
   return (
     <div className="repos-container">
@@ -19,18 +61,16 @@ const ReposContainer = () => {
         Repositories <b>({repositories.length})</b>
       </h1>
 
-      {repositories.map((repos) => {
-        return (
-          <div className="repo-item" key="index">
-            <div className="title">
-              {repos}
-              <button className="btn-arrow">
-                <img src={arrow_icon} alt="Show details" title="Show details" />
-              </button>
-            </div>
+      {repositories.map((repo) => (
+        <div className="repo-item">
+          <div className="title">
+            {repo["name"]}{" "}
+            <button className="btn-arrow" title="Show details">
+              <img src={arrow_icon} />
+            </button>
           </div>
-        );
-      })}
+        </div>
+      ))}
     </div>
   );
 };
